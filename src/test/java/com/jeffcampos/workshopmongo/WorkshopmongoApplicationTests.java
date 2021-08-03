bbba73a7135d6cb2d957982e5481497a5c4fdbe0
Admin <admin@jeff-campos.local>
2021-08-03 07:40:06 -0300

    Initial commit

---
+++ src/test/java/com/jeffcampos/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeffcampos.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
