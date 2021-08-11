af140d451d22f0d2445d0a983599b3f17244662f
Admin <admin@jeff-campos.local>
2021-08-11 08:11:45 -0300

    Adicionando dominio Post

--- src/main/java/com/jeffcampos/workshopmongo/config/Instantiation.java
+++ src/main/java/com/jeffcampos/workshopmongo/config/Instantiation.java
@@ -1,31 +1,45 @@
 package com.jeffcampos.workshopmongo.config;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.jeffcampos.workshopmongo.domain.Post;
 import com.jeffcampos.workshopmongo.domain.User;
+import com.jeffcampos.workshopmongo.repository.PostRepository;
 import com.jeffcampos.workshopmongo.repository.UserRepository;
-
 
 @Configuration
 public class Instantiation implements CommandLineRunner {
 
 	@Autowired
 	UserRepository userRepository;
+	
+	@Autowired
+	PostRepository postRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
 
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+
 		userRepository.deleteAll();
+		postRepository.deleteAll();
 
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 
+		Post post1 = new Post(null, sdf.parse("10/08/2021"), "Partiu viagem", "Vou viajar para São Rock", maria);
+		Post post2 = new Post(null, sdf.parse("11/08/2021"), "Bundinha", "Acordei em São Rock", maria);
+
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		postRepository.saveAll(Arrays.asList(post1, post2));
 	}
 
 }
---
+++ src/main/java/com/jeffcampos/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,9 @@
+package com.jeffcampos.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.jeffcampos.workshopmongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {}
