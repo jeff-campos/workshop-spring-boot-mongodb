fda6c8b32d438466718564362d8b94b392242af7
Admin <admin@jeff-campos.local>
2021-08-12 08:10:22 -0300

    Obtendo post por ID

---
+++ src/main/java/com/jeffcampos/workshopmongo/services/PostService.java
@@ -0,0 +1,23 @@
+package com.jeffcampos.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.jeffcampos.workshopmongo.domain.Post;
+import com.jeffcampos.workshopmongo.repository.PostRepository;
+import com.jeffcampos.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+}
